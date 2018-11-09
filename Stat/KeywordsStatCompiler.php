eed9a32dee68cfa0833cb160d0d82340238a89de
Louis Sicard <lsicard@core-techs.fr>
2018-11-09 20:59:16 +0100

    keywords stat

--- Stat/KeywordsStatCompiler.php
+++ Stat/KeywordsStatCompiler.php
@@ -4,18 +4,19 @@ namespace AdimeoDataSuite\Stat;
 
 use AdimeoDataSuite\Index\StatIndexManager;
 
-class AvgResponseTimeStatCompiler extends StatCompiler
+
+class KeywordsStatCompiler extends StatCompiler
 {
   function getDisplayName()
   {
-    return "Average response time by period";
+    return "Keywords statistics";
   }
 
   function compile($mapping, $from, $to, $period)
   {
     $query = '
-    {
-        "query": {
+      {
+      "query": {
           "bool": {
               "must": [{
                 "term": {
@@ -23,22 +24,26 @@ class AvgResponseTimeStatCompiler extends StatCompiler
                 }
               }]
           }
-        },
-        "aggs": {
-            "date": {
-                "date_histogram": {
-                    "field": "date",
-                    "interval": "' . $this->getElasticPeriod($period) . '"
+      },
+      "aggs": {
+          "keywords": {
+              "nested":{
+                "path": "query"
+              },
+              "aggs":{
+                "raw": {
+                  "terms": {
+                      "field": "query.raw"
+                  }
                 },
-                "aggs": {
-                    "avg_response_time": {
-                        "avg": {
-                            "field": "response_time"
-                        }
-                    }
+                "analyzed": {
+                  "terms": {
+                      "field": "query.analyzed"
+                  }
                 }
-            }
-        }
+              }
+          }
+      }
     }';
     $query = json_decode($query, TRUE);
     if($from != null) {
@@ -53,21 +58,22 @@ class AvgResponseTimeStatCompiler extends StatCompiler
     if($to != null) {
       $query['query']['bool']['must'][] = json_decode('{
                     "range": {
-                        "date": {
+                        "stat_date": {
                             "lte": "' . $to->format('Y-m-d\TH:i') . '"
                         }
                     }
                 }', TRUE);
     }
+    $query = json_encode($query, JSON_PRETTY_PRINT);
 
     $res = $this->getStatIndexManager()->search(StatIndexManager::APP_INDEX_NAME, $query, 0, 9999, 'stat');
 
-    if(isset($res['aggregations']['date']['buckets'])){
+    if(isset($res['aggregations']['keywords']['analyzed']['buckets'])){
       $data = array();
-      foreach($res['aggregations']['date']['buckets'] as $bucket){
+      foreach($res['aggregations']['keywords']['analyzed']['buckets'] as $bucket){
         $data[] = array(
-          \DateTime::createFromFormat('Y-m-d\TH:i:s.000\Z', $bucket['key_as_string'])->format('Y-m-d H:i'),
-          $bucket['avg_response_time']['value']
+          $bucket['key'],
+          $bucket['doc_count']
         );
       }
       $this->setData($data);
@@ -77,19 +83,19 @@ class AvgResponseTimeStatCompiler extends StatCompiler
 
   function getHeaders()
   {
-    return array('Date/time', 'Average response time');
+    return array('Keywords', 'Count');
   }
 
   function getGoogleChartClass()
   {
-    return 'google.visualization.LineChart';
+    return 'google.visualization.ColumnChart';
   }
 
   function getJSData()
   {
     $js = 'var statData = new google.visualization.DataTable();
-    statData.addColumn("datetime", "Date/time");
-    statData.addColumn("number", "Average response time");
+    statData.addColumn("string", "Keywords");
+    statData.addColumn("number", "Count");
 
     statData.addRows([';
 
@@ -97,17 +103,17 @@ class AvgResponseTimeStatCompiler extends StatCompiler
     //Data
     foreach($this->getData() as $data){
       if($data[0] != null && !empty($data[0]) && $data[1] != null && !empty($data[1])) {
-        if(!$first)
+        if (!$first)
           $js .= ',';
         $first = false;
-        $js .= '[new Date("' . $data[0] . '"), ' . $data[1] . ']';
+        $js .= '["' . $data[0] . '", ' . $data[1] . ']';
       }
     }
 
     $js .= ']);';
 
     $js .= 'var chartOptions = {
-          title: "Average response time",
+          title: "Keywords statistics",
           legend: { position: "bottom" }
         };';
     return $js;
